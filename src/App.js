c52858504fb128861d4e7ad126a5516111ef6ba1
Giovana Birck <gmattosbirck@my.bcit.ca>
2025-02-03 10:08:13 -0800

    click to regular time

--- src/App.js
+++ src/App.js
@@ -7,8 +7,20 @@ function App() {
     minutes: 0,
     seconds: 0,
   });
-
+  const [regularTime, setRegularTime] = useState(""); // Add state for regular time
   const [showInfoBox, setShowInfoBox] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [isWhiteScreen, setIsWhiteScreen] = useState(false);
+  const [isGlitching, setIsGlitching] = useState(false); // State to control glitch animation
+
+  const handleScreenFlip = () => {
+    setIsGlitching(true); // Trigger the glitch effect
+    setIsWhiteScreen((prevState) => !prevState); // Immediately flip the screen
+
+    setTimeout(() => {
+      setIsGlitching(false); // End glitch effect after the glitch animation
+    }, 800); // Delay for glitch effect duration
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -36,63 +48,120 @@ function App() {
         minutes: digitalMinutes,
         seconds: digitalSeconds,
       });
+
+      setRegularTime(now.toLocaleTimeString("en-GB", { hour12: false }));
     }, 1000);
 
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setCursorPosition({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
   return (
-    <div className="App">
-      <div className="header-container">
+    <div 
+      className={`App ${isWhiteScreen ? "white-screen" : ""} ${isGlitching ? "glitch-transition" : ""}`}      
+      onClick={handleScreenFlip}
+    >
+      {!isWhiteScreen && (
+        <>
+          <div 
+            className="floating-text" 
+            style={{ top: cursorPosition.y + 10, left: cursorPosition.x + 10 }}
+          >
+            Click anywhere to access the real world
+          </div>
+
+          <div className="header-container">
+            <div 
+              className="project-name"
+              onMouseEnter={() => setShowInfoBox(true)}
+              onMouseLeave={() => setShowInfoBox(false)}
+            >
+              <h1>The Digital Day</h1>
+            </div>
+            <div>
+              <h2>Would you consider changing how we specify time?</h2>
+            </div>
+
+            <div className={`info-box ${showInfoBox ? "visible" : ""}`}>
+              <p>
+                "The current system is arbitrary. The day is divided into twenty-four rather arbitrary but standard units--hours. 
+                But we tell time in units of twelve, not twenty-four, so there have to be two cycles of twelve hours each, plus 
+                the special conversion of a.m. and p.m. so we know which cycle we are talking about. Then we divide each hour 
+                into sixty minutes and each minute into sixty seconds.
+              </p>
+              <p>
+                What if we switched to metric divisions: seconds divided into tenths, milliseconds, and microseconds? We would 
+                have days, millidays, and microdays. There would have to be a new hour, minute, and second: call them the digital 
+                hour, the digital minute, and the digital second. <span>It would be easy: ten digital hours to the day, one 
+                hundred digital minutes to the digital hour, one hundred digital seconds to the digital minute.</span>
+              </p>
+              <p>
+                Each digital hour would last exactly 2.4 times an old hour: 144 old minutes. So the old one-hour period of the 
+                schoolroom or television program would be replaced with a half-digital hour period, or 50 digital minutes--only 
+                20 percent longer than the current hour. We could adapt to the differences in duration with relative ease."
+              </p>
+              <p>
+                - The Design of Everyday Things, Don Norman (1988)
+              </p>
+            </div>
+          </div>
+
+          <div className="digital-display">
+            <p>
+              {String(digitalTime.hours).padStart(2, "0")}:
+              {String(digitalTime.minutes).padStart(2, "0")}:
+              {String(digitalTime.seconds).padStart(2, "0")}
+            </p>
+          </div>
+
+          <footer>
+            <p>
+              created by 
+              <br></br>
+              <a href="https://www.giovanabirck.com/" target="_blank">Giovana Birck</a></p>
+          </footer>
+        </>
+      )}
+
+      {isWhiteScreen && (
         <div 
-          className="project-name"
-          onMouseEnter={() => setShowInfoBox(true)}
-          onMouseLeave={() => setShowInfoBox(false)}
+          className="floating-text" 
+          style={{ top: cursorPosition.y + 10, left: cursorPosition.x + 10 }}
         >
-          <h1>The Digital Day</h1>
+          Take me back to the digital day
         </div>
-        <div>
-          <h2>Would you consider changing how we specify time?</h2>
+      )}
+
+      {isWhiteScreen && (
+        <div className="regular-time-display">
+          <div className="header-container">
+            <div className="project-name">
+              <h1>The Real World</h1>
+            </div>
+            <div>
+              <h2>Just the regular stuff</h2>
+            </div>
+          </div>
+          <div className="digital-display">
+            <p>{regularTime}</p>
+          </div>
+          <footer>
+            <p>
+              created by 
+              <br></br>
+              <a className="a-normal-stuff" href="https://www.giovanabirck.com/" target="_blank">Giovana Birck</a></p>
+          </footer>
         </div>
-
-        <div className={`info-box ${showInfoBox ? "visible" : ""}`}>
-          <p>
-            "The current system is arbitrary. The day is divided into twenty-four rather arbitrary but standard units--hours. 
-            But we tell time in units of twelve, not twenty-four, so there have to be two cycles of twelve hours each, plus 
-            the special conversion of a.m. and p.m. so we know which cycle we are talking about. Then we divide each hour 
-            into sixty minutes and each minute into sixty seconds.
-          </p>
-          <p>
-            What if we switched to metric divisions: seconds divided into tenths, milliseconds, and microseconds? We would 
-            have days, millidays, and microdays. There would have to be a new hour, minute, and second: call them the digital 
-            hour, the digital minute, and the digital second. <span>It would be easy: ten digital hours to the day, one 
-            hundred digital minutes to the digital hour, one hundred digital seconds to the digital minute.</span>
-          </p>
-          <p>
-            Each digital hour would last exactly 2.4 times an old hour: 144 old minutes. So the old one-hour period of the 
-            schoolroom or television program would be replaced with a half-digital hour period, or 50 digital minutes--only 
-            20 percent longer than the current hour. We could adapt to the differences in duration with relative ease."
-          </p>
-          <p>
-            - The Design of Everyday Things, Don Norman (1988)
-          </p>
-        </div>
-      </div>
-
-      <div className="digital-display">
-        <p>
-          {String(digitalTime.hours).padStart(2, "0")}:
-          {String(digitalTime.minutes).padStart(2, "0")}:
-          {String(digitalTime.seconds).padStart(2, "0")}
-        </p>
-      </div>
-
-      <footer>
-        <p>
-          created by 
-          <br></br>
-          <a href="https://www.giovanabirck.com/" target="_blank">Giovana Birck</a></p>
-      </footer>
+        
+      )}
     </div>
   );
 }
